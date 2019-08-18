731b5c9a9dabf5eafb9395808a8c99df522e824a
Adrian-Montemayor <adrianmoncar@hotmail.com>
2019-08-18 18:44:22 -0500

    added infinite loop

--- LoopExamples/main.cpp
+++ LoopExamples/main.cpp
@@ -29,5 +29,13 @@ int main()
         cin >> selection;
     }while(selection!='0');
     
+    while (true) {
+        char again {};
+        cout << "Do you want to loop again? (Y/N):";
+        cin >> again;
+        
+        if (again == 'N' || again == 'n')
+            break;
+    }
 	return 0;
 }
